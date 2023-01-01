cc48bfdea86e03ffd8859711c8ebfc34ad09a7d7
stucombs <stu.combs@icloud.com>
2023-01-01 13:56:16 -0600

    Update view mode css

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
 	//REDIRECT TO HTTPS IF CONNECTING OVER PORT 80 [HTTP]
-	if($_SERVER['SERVER_PORT'] !== '443'){
-		header('Location: https://stucombs.com');
-	}
+	if($_SERVER['SERVER_PORT'] !== '443') header('Location: https://stucombs.com')
 ?>
 <!DOCTYPE html>
 <html>
@@ -13,7 +11,7 @@
 		<meta http-equiv="Content-Language" content="en">
 		<meta name="description" content="A professional website by Stu Combs">
 		<meta name="keywords" content="developer, CV, resume, Combs, Stuart, Stu, job, hire, web developer, full stack">
-		<meta name="author" content="stucombs">
+		<meta name="author" content="Stu Combs">
 		<title id="page_title">Welcome to Stu Combs' Website</title>
 		<!--load all styles-->
 		<link href="./fontawesome/css/all.min.css" rel="stylesheet" />
@@ -21,7 +19,7 @@
 		<link rel="stylesheet" type='text/css' href='./index.css' />
 		<link href="https://fonts.googleapis.com/css?family=Raleway|Roboto|Source+Code+Pro|Source+Sans+Pro&display=swap" rel="stylesheet" />
 	</head>
-	<body id="page_main_body" name="page_main_body" class="dark-mode">
+	<body id="page_main_body" name="page_main_body">
 		<!-- BEGIN MAIN PAGE-->
 		<div id="main-div" class="container-fluid main-div">
 			<div class="row spacer">
@@ -39,12 +37,12 @@
 			</div>
 			<div class="row spacer">
 				<div class="col-xl-12">
-					<span class="inline-text">I am a full stack web developer from Indianapolis, Indiana. Most of my past and current work is in Linux, Apache, MySQL and PHP environments with React.js frontend and experience in Amazon Web Services.</span>
+					<span class="inline-text">I am a full stack web developer from Chicago. Most of my past and current work is in Linux, MySQL and PHP environments with React and Angular frontends and experience in Amazon Web Services.</span>
 				</div>
 			</div>
 			<div class="row spacer">
 					<div class="col-xl-12">
-						<span class="inline-text">Raised in the Hoosier state, I graduated with an Informatics Degree from the School of Informatics and Computing at Indiana University Bloomington. Outside of developing, I enjoy videogames, spending my time at the gym or trying a new beer.</span>
+						<span class="inline-text">Raised in Indiana, I graduated with an Informatics Degree from the School of Informatics and Computing at Indiana University Bloomington. Outside of developing, I enjoy videogames, spending my time at the gym or trying a new beer.</span>
 					</div>
 			</div>
 			<div class="row">
@@ -54,23 +52,30 @@
 			</div>
 			<div class="row">
 				<div class="col-xl-12" align="center">
-					<i class="fab fa-linkedin fa-3x external-icon" style="color: #0F77B5;" onclick="window.open('https://linkedin.com/in/combsstuart')" title="https://linkedin.com/in/combsstuart"></i><span style="font-size: 45px">&nbsp;&nbsp;|
-						&nbsp;&nbsp;</span><i class="fab fa-github fa-3x external-icon" onclick="window.open('https://github.com/stucombs')" title='https://github.com/stucombs'></i><span style="font-size: 45px">&nbsp;&nbsp;|
-						&nbsp;&nbsp;</span><a href="mailto:stu.combs@icloud.com"><i class="far fa-envelope fa-3x external-icon" title="Email Stu"></i></a>
+					<i class="fab fa-linkedin fa-3x external-icon" style="color: #0F77B5;" onclick="window.open('https://linkedin.com/in/combsstuart')" title="https://linkedin.com/in/combsstuart"></i>
+					<span style="font-size: 45px">&nbsp;|&nbsp;</span>
+					<i class="fab fa-github fa-3x external-icon" onclick="window.open('https://github.com/stucombs')" title='https://github.com/stucombs'></i><span style="font-size: 45px">
 				</div>
 			</div>
 		</div>
 		<!-- JAVASCRIPT -->
 		<script>
-			let light_mode = true;
+			let light_mode;
+			// On initial load determine default color scheme
+			if( window.matchMedia('(prefers-color-scheme: dark)').matches ) {
+				light_mode = false;
+				onSwitchMode();
+			} else {
+				light_mode = true;
+				onSwitchMode();
+			}
+			
 			function onSwitchMode(){
 				if( light_mode ){
-					document.getElementById('page_main_body').classList.add('light-mode');
-					document.getElementById('page_main_body').classList.remove('dark-mode');
+					document.documentElement.removeAttribute("dark");
 					document.getElementById('view_mode_status').className = "fas fa-sun fa-2x status-button";
 				}else{
-					document.getElementById('page_main_body').classList.add('dark-mode');
-					document.getElementById('page_main_body').classList.remove('light-mode');
+					document.documentElement.setAttribute("dark", true);
 					document.getElementById('view_mode_status').className = "far fa-moon fa-2x status-button";
 				}
 				light_mode = !light_mode
